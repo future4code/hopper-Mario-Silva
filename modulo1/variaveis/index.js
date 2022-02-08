7f1aadca4d703d3d0810a25660b064da0ac7d4ad
mario <mario_fernando777@hotmail.com>
2022-02-08 20:08:13 -0300

    Exercicio variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,64 @@
+// Exercicíos de interpretação. 
+
+// 1 - Resposta: a = 10 b = 5 (let permite mudar o valor)
+
+// 2 -  Resposta: c = 10  b = 10  a = 10 
+
+// 3
+
+/*let HorasTrabalhadasDia = prompt("Quantas horas você trabalha por dia?")
+let ValorRecebidoDia = prompt("Quanto você recebe por dia?") 
+alert("Você recebe ${ValorRecebidoDia/HorasTrabalhadasDia")*/
+
+// Exercicíos de escrita de código.
+
+// 1
+
+/*let nome 
+console.log(typeof nome) 
+let idade 
+console.log(typeof idade)*/
+
+// Retornou "UNDEFINED" porque não foi colocado nenhum valor nas variáveis.
+
+/*const nomeDoUsuario = prompt("Qual o seu nome?")
+const idadeDoUsuario = prompt("Qual a sua idade?")
+console.log (typeof nomeDoUsuario)
+console.log (typeof idadeDoUsuario)*/
+
+// Retornou "STRING" sendo que em "nomeDoUsuario" está correto por ser texto, já em "IdadeDoUsuario" deve ser transformando em "NUMBER".
+
+/*const nomeDoUsuario = "Mario"
+const idadeDoUsuario = 36
+console.log("Olá" , nomeDoUsuario, "você tem" , idadeDoUsuario , "anos de idade")*/
+
+// 2
+
+/*let tempo = prompt("Está chovendo hoje?")
+let guardaChuva = prompt("Você trouxe guarda-chuva?")
+let aula = prompt("Hoje tem aula?")
+
+tempo = "Sim"
+guardaChuva = "Não"
+aula = "Não"
+console.log("Esta chovendo?" , tempo , "Você trouxe guarda-chuva?" , guardaChuva , "Hoje tem aula?", aula)*/ 
+
+// 3
+
+/*let a = 10
+let b = 25
+let c = 10
+
+a = b
+b = c
+console.log(a, b)*/
+
+//DESAFIO
+
+/*const primeiroNumero = Number(prompt("Digite o primeiro número"))
+const segundoNumero = Number(prompt("Digite o segundo número"))
+console.log("O primeiro número somado ao segundo número resulta em" , primeiroNumero + segundoNumero)
+console.log("O primeiro número multiplicado pelo segundo número resulta em" , primeiroNumero * segundoNumero)*/
+
+
+
