5cbe25826bf34f52cb06c464c3ba59ddc2e4456d
Mario <mario_fernando777@hotmail.com>
2022-02-16 22:53:47 -0300

    Respondendo projeto-lista-js

--- modulo1/projeto-lista-js/exercicios.js
+++ modulo1/projeto-lista-js/exercicios.js
@@ -19,13 +19,20 @@ function imprimeMensagem() {
 // EXERCÍCIO 01
 function calculaAreaRetangulo() {
   // implemente sua lógica aqui
-
+ const altura = Number(prompt("Digite a altura"))
+ const largura = Number(prompt("Digite a largura"))
+ const calculaAreaRetangulo = altura*largura
+ console.log(calculaAreaRetangulo) 
 }
 
 // EXERCÍCIO 02
 function imprimeIdade() {
   // implemente sua lógica aqui
 
+const anoAtual = Number(prompt("Digite o ano atual"))
+const anoNascimento = Number(prompt("Digite o ano de nascimento"))
+const imprimeIdade = anoAtual - anoNascimento
+console.log (imprimeIdade)
 }
 
 // EXERCÍCIO 03
@@ -38,18 +45,32 @@ return calculaIMC = peso / (altura*altura)
 function imprimeInformacoesUsuario() {
   // implemente sua lógica aqui
   // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+  
+  const NOME = prompt("Digite seu nome")
+  const IDADE = prompt("Digite sua idade")
+  const EMAIL = prompt("Digite seu e-mail")
+  const imprimeInformacoesUsuario = (`Meu nome é ${NOME}, tenho ${IDADE} anos, e o meu email é ${EMAIL}.`)
+  console.log(imprimeInformacoesUsuario)
+ 
 
 }
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
   // implemente sua lógica aqui
+  const cor1 = prompt("Digite a primeira cor")
+  const cor2 = prompt("Digite a segunda cor")
+  const cor3 = prompt("Digite a terceira cor")
+  const imprimeTresCoresFavoritas = [cor1, cor2, cor3]
+  console.log(imprimeTresCoresFavoritas)
+  
 
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
+return string.toUpperCase()
 
 }
 
@@ -63,24 +84,29 @@ function calculaIngressosEspetaculo(custo, valorIngresso) {
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
+  return string1.length === string2.length
 
 }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
   // implemente sua lógica aqui
+return array[0]
 
 }
 
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
   // implemente sua lógica aqui
-
+return array[array.length -1]
 }
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
+array.splice(to,0)
+
+
 
 }
 
