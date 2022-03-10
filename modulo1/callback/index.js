37a93e5b656b7056930d2889410b6cee65562acb
Mario <mario_fernando777@hotmail.com>
2022-03-10 19:17:03 -0300

    Entrega Lacos

--- modulo1/callback/index.js
+++ modulo1/callback/index.js
@@ -172,27 +172,27 @@
 
 // e) Crie um novo array onde cada item é uma frase "Compre [NOME] por [PREÇO]". Esse array deve conter frases apenas dos itens cujo nome contenha a palavra "Ypê"
 
-// const produtos = [
-//    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
-//    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
-//    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
-//    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
-//    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
-//    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
-//    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
-//    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
-//    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
-//    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
-// ]
+const produtos = [
+   { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+   { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+   { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+   { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+   { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+   { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+   { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+   { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+   { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+   { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+]
 
 
-// const onlyYpe = produtos.filter((item) => {
-//    return item.nome.includes('Ypê')
-// })
+const onlyYpe = produtos.filter((item) => {
+   return item.nome.includes('Ypê')
+})
 
-// const inicio = (produto) =>{
-//    console.log(`Compre ${produto.nome} por ${produto.preco}`)
-// }
+const inicio = (produto) =>{
+   console.log(`Compre ${produto.nome} por ${produto.preco}`)
+}
 
-// const outroGato = onlyYpe.map(inicio)
-// inicio(onlyYpe)
+const outroGato = onlyYpe.map(inicio)
+inicio(onlyYpe)
